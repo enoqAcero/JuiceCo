71ee82631f9bfc158109be0f603ca11075e066c1
Eduardo Estrella <eduardofloresestr@gmail.com>
2024-03-27 15:54:02 -0600

    Progres save fixed

--- Scripts/vehicles.gd
+++ Scripts/vehicles.gd
@@ -1,6 +1,6 @@
 extends ScrollContainer
 
-var player = load("res://Save/PlayerSave.tres")
+
 var item_scene = preload("res://Scenes/GarageItem.tscn")
 @onready var main_container = $VBoxContainer
 
@@ -9,15 +9,15 @@ func fill_vehicle_list():
 	for item in main_container.get_children():
 		item.queue_free()
 	
-	for i in range(player.Vehicles.size()):
+	for i in range(GlobalVariables.player.Vehicles.size()):
 		
-		var vehicle_id = player.Vehicles[i]
+		var vehicle_id = GlobalVariables.player.Vehicles[i]
 		var item = item_scene.instantiate()
 		main_container.add_child(item)
 		item.slot = i
 		
 		if vehicle_id != 0:
-			var vehicle = player.Transport[vehicle_id - 1]
+			var vehicle = GlobalVariables.player.Transport[vehicle_id - 1]
 			item.data = vehicle
 			item.get_node("Panel").visible = true
 			item.image.texture = vehicle.skin
@@ -25,7 +25,7 @@ func fill_vehicle_list():
 			item.capacity.text = "Capacity: " + str(vehicle.capacity)
 			item.cost.visible = false
 			item.upgrade_button.pressed.connect(Callable(_on_upgrade_button_pressed).bind(item.slot, item))
-		elif i == 0 or (i > 0 and player.Vehicles[ i-1 ] != 0):
+		elif i == 0 or (i > 0 and GlobalVariables.player.Vehicles[ i-1 ] != 0):
 			item.get_node("PanelBuyVehicle/Button").pressed.connect(Callable(_on_purchase_button_pressed).bind(item.slot))
 			item.get_node("PanelBuyVehicle").visible = true
 		else:
